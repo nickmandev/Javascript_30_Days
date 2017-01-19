8b9abf49a40c429356a7675053c48de36dbeed82
nickmandev <nickmandev@gmail.com>
2017-01-19 17:23:23 +0200

    -Day 11 completed -Day 12 completed -Day 13 completed -Day 14 completed -Day 15 completed -Day 16 completed -Day 17 completed -Day 18 completed -Day 19 completed -Day 20 completed -Day 21 completed -Day 22 completed

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,9 +4,13 @@ const skipForward = document.querySelector('.player__button[data-skip="25"]')
 const skipBackward = document.querySelector('.player__button[data-skip="-10"]')
 const volumeSlider = document.querySelector('.player__slider[name="volume"]')
 const speedSlider = document.querySelector('.player__slider[name="playbackRate"]')
+const progressBar = document.querySelector('.progress__filled')
+const progress = document.querySelector('.progress')
+const ranges = document.querySelectorAll('.player__slider')
 let playing = false
 let clicked = false
 let videoCurrTime;
+
 function toggleButton(){
     if (playing == false) {
         video.play()
@@ -18,12 +22,12 @@ function toggleButton(){
     }
 }
 
-function skip25s(e){
+function skip25s(){
     if(playing == true){
         video.currentTime += 25
     } 
 }
-function skip10s(e){
+function skip10s(){
     if(playing == true) {
         if(video.currentTime > 10){
             video.currentTime -= 10
@@ -31,31 +35,59 @@ function skip10s(e){
     }
 }
 
+function handleRangeChange() {
+    video[this.name] = this.value
+}
+
 function toggleVolume(e){
     if(!clicked){return}
-    if(e.screenX >= 170 || e.screenX <= 70){
-        return
-    } else {
-        video.volume =((e.screenX - 70) / 100)
-        console.log(video.volume)
-    }
     
 }
  
- var arr = [1,2,3,4,5]
-function toggleRate(e) {
+function togglePlaybackRate(e) {
     if(!clicked){return}
+    if (e.screenX >= 320 || e.screenX <= 200){
+        return
+    } else {
+        video.playbackRate = (((e.screenX - 200) / 10) /6)
+    }
+}
+
+function videoSeek(e) {
+    console.log(e)
+}
+
+function updateButton(){
+    const icon = this.paused ? '►' : '❚ ❚'
+    playButton.textContent = icon
     
+}
+
+function handleProgress(){
+    const percent = (video.currentTime / video.duration) * 100
+    progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
+    video.currentTime = scrubTime
+    console.log(e)
 }
 playButton.addEventListener('click', toggleButton)
 video.addEventListener('click', toggleButton)
+video.addEventListener('play', updateButton)
+video.addEventListener('pause', updateButton)
+video.addEventListener('timeupdate', handleProgress)
 skipForward.addEventListener('click',skip25s)
 skipBackward.addEventListener('click',skip10s)
 volumeSlider.addEventListener('mousedown',() => { clicked = true})
 volumeSlider.addEventListener('mousemove',toggleVolume)
 volumeSlider.addEventListener('mouseup',() => {clicked = false})
 speedSlider.addEventListener('mousedown',() => { clicked = true})
-speedSlider.addEventListener('mousemove',toggleRate)
+speedSlider.addEventListener('mousemove',togglePlaybackRate)
 speedSlider.addEventListener('mouseup',() => {clicked = false})
-
-console.table(arr)
+ranges.forEach(range => range.addEventListener('change',handleRangeChange))
+progress.addEventListener('click', scrub)
+progress.addEventListener('mousemove', (e) => clicked && scrub(e))
+progress.addEventListener('mouseup',() => {clicked = false})
+progress.addEventListener('mousedown',() => { clicked = true})
